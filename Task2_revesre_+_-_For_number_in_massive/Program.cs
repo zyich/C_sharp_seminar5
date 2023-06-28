3103be66c3478cd42a3ca8a9a9eb2946efb3b15c
zyich <zuilinho@yandex.ru>
2023-06-28 17:46:23 +0200

    task 2

---
+++ Task2_revesre_+_-_For_number_in_massive/Program.cs
@@ -0,0 +1,31 @@
+﻿/* 
+Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот. 
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+*/ 
+ 
+ 
+ 
+int [] CreateArray(int size, int min, int max){ 
+    int [] array = new int [size]; 
+    Random rand = new Random(); 
+ 
+    for (int i = 0; i < size; i++) 
+    { 
+        array [i] = rand.Next(min, max+1); 
+    } 
+    Console.WriteLine($"[{string.Join(", ", array)}]"); 
+    return array; 
+} 
+ 
+ 
+void ReverseDigints (int [] array){ 
+ 
+    for (int i = 0; i < array.Length; i++) 
+    { 
+        array[i]*=-1;  
+    } 
+    Console.WriteLine($"[{string.Join(", ", array)}]"); 
+} 
+ 
+int[] arr = CreateArray(10,-10,10); 
+ReverseDigints(arr);
